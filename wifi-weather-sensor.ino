2815bfc412481d68fd83b9aa6836beaa31efb0c3
iLLiac4 <illiac4@gmail.com>
2015-08-01 08:41:34 +0200

    Update wifi-weather-sensor.ino

--- wifi-weather-sensor.ino
+++ wifi-weather-sensor.ino
@@ -1,31 +1,34 @@
 /*
- *  Filename : wifi-weather-sensor.ino
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
  *
- *  ESP8266 based wireless weather snesor.
- *  ESP8266 reads data from DHT22, then send it to data.sparkfun.com
- *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
  *
  */
 
 #include <ESP8266WiFi.h>
 #include "DHT.h"
 
-#define DHTPIN 2     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTPIN 0     // GPIO 0 pin of ESP8266
+//#define DHTPIN 2     // GPIO 2 pin of ESP8266
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-const char* ssid       = ".................";
-const char* password   = ".................";
-const char* host       = "data.sparkfun.com";
-const char* publicKey  = ".................";
-const char* privateKey = ".................";
+const char* ssid     = "iLLiac4";
+const char* password = "nonenone";
 
-DHT dht(DHTPIN, DHTTYPE, 30); // 30 is for cpu clock of ESP8266 80Mhz
+const char* host = "data.sparkfun.com";
+const char* publicKey = "n1YLn05qpxTogMLKEj0L";
+const char* privateKey = "ModRmEDbBWTD0qnME2An";
+
+DHT dht(DHTPIN, DHTTYPE,30); // 30 is for cpu clock of esp8266 80Mhz
 
 void setup() {
   Serial.begin(115200);
 
   dht.begin();
-
+  
   delay(10);
 
   // We start by connecting to a WiFi network
@@ -34,16 +37,16 @@ void setup() {
   Serial.println();
   Serial.print("ESP8266 Connecting to ");
   Serial.println(ssid);
-
+  
   WiFi.begin(ssid, password);
-
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
   Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
@@ -54,20 +57,21 @@ void loop() {
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
-  // Read temperature as Celsius
+  // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit
-  float f = dht.readTemperature(true);
-
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(false);
+  
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-
-  // Compute heat index
-  // Must send in temp in Fahrenheit!
-  float hi = dht.computeHeatIndex(f, h);
+  
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
 
   Serial.print("Humidity: ");
   Serial.print(h);
@@ -78,12 +82,14 @@ void loop() {
   Serial.print(f);
   Serial.print(" *F\t");
   Serial.print("Heat index: ");
-  Serial.print(hi);
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
   Serial.println(" *F");
 
   Serial.print("connecting to ");
   Serial.println(host);
-
+  
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   const int httpPort = 80;
@@ -91,35 +97,37 @@ void loop() {
     Serial.println("connection failed");
     return;
   }
-
+  
   // We now create a URI for the request
   String url = "/input/";
   url += publicKey;
   url += "?private_key=";
   url += privateKey;
   url += "&temp=";
-  url += f;
+  url += t;
   url += "&hum=";
   url += h;
   url += "&hidx=";
-  url += hi;
-
+  url += hic;
+  
   Serial.print("Requesting URL: ");
   Serial.println(url);
-
+  
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
+               "Host: " + host + "\r\n" + 
                "Connection: close\r\n\r\n");
   delay(10);
-
+  
   // Read all the lines of the reply from server and print them to Serial
   while(client.available()){
     String line = client.readStringUntil('\r');
     Serial.print(line);
   }
-
+  
   Serial.println();
   Serial.println("closing connection");
-  delay(600000);
+  delay(20*1000); // Send data every 20 seconds
+  // Send to sleep for 2 minutes
+  ESP.deepSleep(2*60000000);
 }
