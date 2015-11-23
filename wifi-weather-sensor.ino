f70316df2b734a87c256ea1363438fd7defcc666
iLLiac4 <illiac4@gmail.com>
2015-11-23 08:54:33 +0100

    Update wifi-weather-sensor.ino

--- wifi-weather-sensor.ino
+++ wifi-weather-sensor.ino
@@ -16,19 +16,19 @@
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 const char* ssid     = "SSID";
-const char* password = "password";
+const char* password = "PASSWORD";
 
 const char* host = "data.sparkfun.com";
-const char* publicKey = ".................";
-const char* privateKey = ".................";
+const char* publicKey = "....................";
+const char* privateKey = "....................";
 
-DHT dht(DHTPIN, DHTTYPE,30); // 30 is for cpu clock of esp8266 80Mhz
+DHT dht(DHTPIN, DHTTYPE, 30); // 30 is for cpu clock of esp8266 80Mhz
 
 void setup() {
   Serial.begin(115200);
 
   dht.begin();
-  
+
   delay(10);
 
   // We start by connecting to a WiFi network
@@ -37,16 +37,16 @@ void setup() {
   Serial.println();
   Serial.print("ESP8266 Connecting to ");
   Serial.println(ssid);
-  
+
   WiFi.begin(ssid, password);
-  
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
@@ -70,9 +70,28 @@ void loop() {
 
   // Compute heat index
   // Must send in temp in Fahrenheit!
+  float   hi = 0.5 * (f + 61.0 + ((f - 68.0) * 1.2) + (h * 0.094));
   
-  float hi = dht.computeHeatIndex(f, h);
-  float hic = dht.convertFtoC(hi);
+  if (hi > 79) {
+    hi = -42.379 +
+             2.04901523 * f +
+            10.14333127 * h +
+            -0.22475541 * f*h +
+            -0.00683783 * pow(f, 2) +
+            -0.05481717 * pow(h, 2) +
+             0.00122874 * pow(f, 2) * h +
+             0.00085282 * f*pow(h, 2) +
+            -0.00000199 * pow(f, 2) * pow(h, 2);
+
+    if((h < 13) && (f >= 80.0) && (f <= 112.0))
+      hi -= ((13.0 - h) * 0.25) * sqrt((17.0 - abs(f - 95.0)) * 0.05882);
+
+    else if((h > 85.0) && (f >= 80.0) && (f <= 87.0))
+      hi += ((h - 85.0) * 0.1) * ((87.0 - f) * 0.2);
+  }
+  float his = (hi - 32) * 0.5555555556;
+
+// Print
 
   Serial.print("Humidity: ");
   Serial.print(h);
@@ -83,7 +102,7 @@ void loop() {
   Serial.print(f);
   Serial.print(" *F\t");
   Serial.print("Heat index: ");
-  Serial.print(hic);
+  Serial.print(his);
   Serial.println(" *C");
 
   Serial.print("connecting to ");
@@ -107,25 +126,25 @@ void loop() {
   url += "&hum=";
   url += h;
   url += "&hidx=";
-  url += hic;
+  url += his;
 
   Serial.print("Requesting URL: ");
   Serial.println(url);
-  
+
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
+               "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
   delay(10);
-  
+
   // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
+  while (client.available()) {
     String line = client.readStringUntil('\r');
     Serial.print(line);
   }
-  
+
   Serial.println();
   Serial.println("closing connection");
-  delay(20*1000); // Send data every 20 seconds
-  ESP.deepSleep(35*60000000); // Send to sleep for 35 minutes
+  delay(20 * 1000); // Send data every 20 seconds
+  ESP.deepSleep(35 * 60000000); // Send to sleep for 35 minutes
 }
